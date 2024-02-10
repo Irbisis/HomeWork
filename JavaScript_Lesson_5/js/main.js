47eafcadaeb099dcc4803f4ba39a0e6dfe1d371e
Irbisis <139844777+Irbisis@users.noreply.github.com>
2024-02-10 11:10:52 +0300

    add JavaScript_Lesson_5

---
+++ JavaScript_Lesson_5/js/main.js
@@ -0,0 +1,57 @@
+// let i = 0
+
+// while(i < 10){
+
+//     i = i + 1
+//     console.log( i )
+// }
+
+// do {
+//     i++
+//     console.log( i )
+// }while(i < 5)
+
+// for(let i = 1; i <= 10; i = i + 2){
+//     console.log( `Пройден ${i} круг` )
+// }
+
+// let officialName = prompt('Какое "официальное" название JavaScript?', '');
+
+// if (officialName == 'ECMAScript') {
+//     alert('Верно!');
+// } else {
+//     alert('Не знаете? “ECMAScript”!');
+// }
+
+// for(let i = 2; i <= 12; i = i + 2){
+//     console.log( ` ${i} ` )
+// }
+
+// let num = 10
+// while (num >=1 ){
+//     console.log( num)
+//     num--
+// }
+
+// Задание 1
+let i = 2;
+while (i <= 12){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+    i++;
+}
+
+// Задание 2
+let num = 10;
+while(num >= 1){
+    console.log(num);
+
+    num--;
+}
+
+
+
+
+
+
