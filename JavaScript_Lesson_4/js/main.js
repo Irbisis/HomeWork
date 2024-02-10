831f41215e0016b6d46c9a9f29c059898da7a710
Irbisis <139844777+Irbisis@users.noreply.github.com>
2024-02-10 10:43:06 +0300

    add template

---
+++ JavaScript_Lesson_4/js/main.js
@@ -0,0 +1,44 @@
+// const firstNumber = 5
+// const secondNumber = 20
+
+// let result = null
+
+// // if(firstNumber == secondNumber){
+
+// //      result = firstNumber + secondNumber
+// //     console.log(result)
+// // }else {
+// //      result = secondNumber - firstNumber
+// //     console.log(result)
+// // }
+// if (firstNumber === 10 && secondNumber === 20) {
+
+//     result = firstNumber + secondNumber
+//     console.log(result)
+// } else {
+//     result = secondNumber - firstNumber
+//     console.log(result)
+// }
+
+// const alex = 'admin'
+
+// if (alex === 'user') {
+//     console.log('Alex is user')
+// } else if (alex === 'vip') {
+//     console.log('Alex is vip user')
+// } else {
+//     console.log('Alex is admin')
+// }
+
+// alex === 'admin' ? console.log('Alex is admin') : console.log('Alex is not admin')
+
+// let age = 37
+
+// if (age >= 18){
+//     console.log('Вы совершеннолетний')
+// }else{
+//     console.log('Вам еще нет 18')
+// }
+
+let age = 37
+age >= 18 ? console.log('Вы совершеннолетний') : console.log('Вам еще нет 18')
