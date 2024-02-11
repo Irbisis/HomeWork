c6550b8cf5a219654fc41044e3aaeb5d7f8dcab9
Irbisis <139844777+Irbisis@users.noreply.github.com>
2024-02-11 16:46:37 +0300

    add JavaScript_Lesson_6

---
+++ JavaScript_Lesson_6/js/main.js
@@ -0,0 +1,15 @@
+// 1
+let numbers = [3, 7, 12, 5, 8, 10, 15, 22];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) { 
+        console.log(numbers[i]); 
+    }
+}
+
+// 2
+let rainbowColors = ['красный', 'оранжевый', 'желтый', 'зеленый', 'голубой', 'синий', 'фиолетовый'];
+
+for (let i = rainbowColors.length - 1; i >= 0; i--) {
+    console.log(rainbowColors[i]); 
+}
